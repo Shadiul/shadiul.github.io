a186e7b9db051fe0d1d95c6ac438b55ae94b9dbd
Shadiul <shadiul.71@gmail.com>
2022-02-15 22:25:56 +0600

    Add some navbar and homepage content

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { PATHS } from "../constants/paths";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="flex justify-end px-16 py-6">
+      <ul className="flex gap-4 font-medium text-xl leading-7">
+        <NavLink to={PATHS.projects}>Projects</NavLink>
+        <NavLink to={PATHS.blogs}>Blogs</NavLink>
+        <NavLink to="#">Contact</NavLink>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
 
 type Props = {};
 
 const Homepage = (props: Props) => {
-  return <div className="bg-orange-500">Homepage</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex px-32 py-16 gap-20">
+        <div className="flex flex-col items-start">
+          <h1 className="text-5xl font-bold leading-snug">
+            Hi, I am Shadiul Huda,
+            <br /> Frontend Developer
+          </h1>
+          <div className="h-10" />
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+          <div className="h-10" />
+          <button className="bg-orange-600 px-5 py-2 text-xl font-medium text-white rounded-sm">
+            Download Resume
+          </button>
+        </div>
+
+        <div>
+          <div className="h-64 w-64 rounded-full bg-gray-400"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
